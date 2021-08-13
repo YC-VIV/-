32bb2aae42f91cf7513eb8a58a0d32fa60fafdc1
yuan <838003282@qq.com>
2021-08-13 23:18:37 +0800

    2021/8/13

--- 动态规划/杨辉三角II.js
+++ 动态规划/杨辉三角II.js
@@ -33,18 +33,48 @@
  var getRow = function(rowIndex) {
     let triangle = []
     let past = []
+    // 思路：将原来二维数组简化为两个一维数组，其中，past用于暂存上一层，triangle用于保存当前层
 
     for( let i = 0 ; i <= rowIndex ; i++ ){
-        past[0] = past[i] = 1
+        // 注意：如果没有在每次循环的时候重置triangle数组，则会在past=triangle的时候，把当前循环时最后一个元素的“1”带到下一次循环的第i-1个past里，导致计算的时候多加了1
+        triangle = new Array(i+1).fill(0)
+
+        // 一头一尾初始值为1
+        triangle[0] = triangle[i] = 1
 
         for( let j = 1 ; j < i ; j ++ ){
-            past[j] = triangle[j-1] + triangle[j]
+            // 注意这里j是从1开始算，到第i-1次执行完后结束（如三角第三行，这里只计算了triangle[1]、triangle[2]）
+            triangle[j] = past[j-1] + past[j]
+
+            // console.log(i+",past["+j+"]："+past[j]+" past is："+past+" triangle is："+triangle)
         }
 
-        triangle = past
+        // 目前的层数就是下一次循环的上一层
+        past = triangle
+
+        // console.log(past)
     }
     
     return triangle
 };
 
+// 终极简化版，简化为一个一维数组，思路看注释运行过程
+var getRow2 = function(rowIndex) {
+    
+    let triangle = new Array(rowIndex+1).fill(0)
+    triangle[0] = 1
+
+    for( let i = 0 ; i <= rowIndex ; i++ ){
+        for(let j = i ; j > 0 ; j-- ){
+            triangle[j] = triangle[j-1] + triangle[j]
+            // console.log(i+" "+j+" "+triangle)
+        }
+    }
+
+    return triangle
+
+}
+
+// getRow(5)
 console.log(getRow(5))
+console.log(getRow2(5))
